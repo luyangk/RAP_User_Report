7b37ca0e602870eb21494ce0d0bd47afd8ffc5db
Lucca Lu <luyangk@gmail.com>
2018-01-10 13:48:58 +0800

    bak 20180110

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var url = require('url');
 
 
 // create server
-http.createServer( function (request, response) {  
+http.createServer( function (request, response) {
    // parse request include path name
    var pathname = url.parse(request.url).pathname;
-   
+
    // output file name
    console.log("Request for " + pathname + " received.");
-   
+
    // read file content from file system
    fs.readFile(pathname.substr(1), function (err, data) {
       if (err) {
@@ -18,17 +18,17 @@ http.createServer( function (request, response) {
          // HTTP status: 404 : NOT FOUND
          // Content Type: text/plain
          response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{	         
+      }else{
          // HTTP status: 200 : OK
          // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         
+         response.writeHead(200, {'Content-Type': 'text/html'});
+
          // response file content
-         response.write(data.toString());		
+         response.write(data.toString());
       }
       // send response file
       response.end();
-   });   
+   });
 }).listen(1442);
 
 // console information
